6b5e3b3c673e663f67242c5f8fddc9b576c459e6
RenanHenriqueSENAI2RPNET <renanoliveiratm@hotmail.com>
2023-12-06 21:44:22 -0300

    feat: removed Flow's API and Front-endand start README

--- Código/BACK/FlowAPIProject/FlowAPIApp/models/sensorModel.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class FlowSensor(models.Model):
-    tempo_operacao = models.DateTimeField()
-    litros_totais = models.FloatField()
-    litros_por_minuto = models.FloatField()
